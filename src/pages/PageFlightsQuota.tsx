3394e0a91fcef672be77933bfc8aba6751c873d5
Ilya Shevchuk <azymazy@gmail.com>
2024-10-17 17:42:13 +0400

    abs paths

--- src/pages/PageFlightsQuota.tsx
+++ src/pages/PageFlightsQuota.tsx
@@ -1,3 +1,7 @@
+import { QUERY_KEYS } from '../shared/consts'
+
+console.log('QUERY_KEYS', QUERY_KEYS)
+
 export function PageFlightsQuota() {
-  return <h1>PageFlightsQuota</h1>
+  return <h1>bup</h1>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/flights-quota-modal',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
