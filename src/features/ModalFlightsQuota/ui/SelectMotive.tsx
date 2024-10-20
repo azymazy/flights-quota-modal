de9f71db23ef630128cafdfb976eb7c2b1664629
Ilya Shevchuk <azymazy@gmail.com>
2024-10-21 00:27:17 +0400

    SelectMotive styles

--- src/features/ModalFlightsQuota/ui/SelectMotive.tsx
+++ src/features/ModalFlightsQuota/ui/SelectMotive.tsx
@@ -4,10 +4,23 @@ import {
   OutlinedInput,
   Select,
   Stack,
+  SelectChangeEvent,
 } from '@mui/material'
 
-export function SelectMotive({ disabled, options, value, onChange }) {
-  const handleChange = (event) => {
+type SelectMotiveProps = {
+  disabled: boolean
+  options: string[]
+  value: string
+  onChange: (value: string) => void
+}
+
+export function SelectMotive({
+  disabled,
+  options,
+  value,
+  onChange,
+}: SelectMotiveProps) {
+  const handleChange = (event: SelectChangeEvent<string>) => {
     onChange(event.target.value)
   }
 
